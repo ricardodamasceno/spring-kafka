3fad1dc320f2551b6a89fc272fe8a8dced47ae41
Ricardo Damasceno <ricardodamascenno@gmail.com>
2021-02-26 17:52:49 -0300

    adjustment on sonsumer

--- src/main/java/com/br/kafka/service/UserConsumerService.java
+++ src/main/java/com/br/kafka/service/UserConsumerService.java
@@ -1,10 +1,17 @@
 package com.br.kafka.service;
 
 import com.br.kafka.config.KafkaPropertiesConfig;
+import com.br.kafka.vo.UserRequestVO;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -18,10 +25,27 @@ public class UserConsumerService {
         var records = kafkaService.consumeMessages(KafkaPropertiesConfig.getUserConsumerProperties(), KafkaPropertiesConfig.USER_TOPIC_NAME);
 
         if(!records.isEmpty()){
+            getUserList(records);
             printRecords(records);
         } else {
             log.error("No records found");
         }
+    }
+
+    private List<UserRequestVO> getUserList(ConsumerRecords records){
+        Gson gson = new GsonBuilder().create();
+        List<UserRequestVO> users = new ArrayList<>();
+
+        records.forEach( user -> {
+            try{
+                String userString = String.valueOf(((ConsumerRecord)user).value());
+                users.add(gson.fromJson(userString, UserRequestVO.class));
+            } catch (Exception e){
+                log.error("Failed to parse string to Json. Exception: " + e.getMessage());
+            }
+        });
+
+        return users;
     }
 
     private void printRecords(ConsumerRecords<String, String> records){
